4f1643aa95d2add596bc485220e56be7d1354571
Yeremia Joy <yeremia.joy@mekari.com>
2025-02-26 10:56:40 +0700

    change viewport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=0.2, maximum-scale=1.0, user-scalable=no',
+    }
+  }
 })
